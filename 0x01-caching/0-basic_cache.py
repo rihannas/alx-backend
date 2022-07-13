8a41a9d77b4602876362539dfd358638e3c09b34
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-14 02:04:45 +0300

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Module for class BasicCache """
+
+from base_caching import BaseCaching
+
+
+"""
+You must use self.cache_data -dict from the parent class BaseCaching
+This caching system doesn’t have limit
+def put(self, key, item):
+Must assign to the dict self.cache_data the item value for the key key.
+If key or item is None, this method should not do anything.
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in self.cache_data,
+return None.
+"""
+
+
+class BasicCache(BaseCaching):
+    """ inherits from BaseCaching and is a caching system """
+
+    def put(self, key, item):
+        """ 
+        Add an item in the cache
+        """
+        if key == None:
+            return None
+
+        elif key not in self.cache_data:
+            return None
+
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key == None:
+            return None
+
+        elif key not in self.cache_data:
+            return None
+
+        else:
+            return self.cache_data[key]
