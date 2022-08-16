1d8dd1bf602af0f2f45001ef1d1c167d1411eaf7
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-08-17 01:40:59 +0300

    task 6

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,14 @@
+import kue from 'kue'
+const queue = kue.createQueue()
+
+const jobData = {
+  phoneNumber: "777777",
+  message: "what's up?",
+}
+
+const job = queue.create('push_notification_code', jobData).save( function(err){
+  if( !err ) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => console.log('Notification job completed'));
+job.on('failed', () => console.log('Notification job failed'));
